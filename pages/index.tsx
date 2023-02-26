224fbae8799fd597384ffc35b89eb7d3874fd053
shahariar50 <shahariar.touhid@gmail.com>
2023-02-27 04:32:46 +0600

    added contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import MainLayout from "components/layout/MainLayout";
 import AboutSection from "components/page/homepage/AboutSection";
 import AdmissionSection from "components/page/homepage/AdmissionSection";
 import ClientSection from "components/page/homepage/Clientsection";
+import ContactSection from "components/page/homepage/ContactSection";
 import DetermineSection from "components/page/homepage/DetermineSection";
 import HeroSection from "components/page/homepage/HeroSection";
 import WhySection from "components/page/homepage/WhySection";
@@ -17,6 +18,7 @@ export default function Home() {
         <DetermineSection />
       </div>
       <ClientSection />
+      <ContactSection />
     </MainLayout>
   );
 }
